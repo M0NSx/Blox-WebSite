4458c1acb1a5724d3f457306bdeeca5ae3a6f840
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-09-23 19:48:47 -0300

    Update login.js

--- source/src/pages/login.js
+++ source/src/pages/login.js
@@ -2,3 +2,5 @@ import Style from './LogIn.module.css'
 import React, { useState } from "react";
 import axios from 'axios';
 import { Link, useNavigate } from "react-router-dom";
+
+export function LogIn() {
