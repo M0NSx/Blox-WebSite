e04ad7d7487f425969946f22662197dbf82fa3d8
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-09-24 18:03:24 -0300

    Update dashboard.js

--- source/src/pages/dashboard.js
+++ source/src/pages/dashboard.js
@@ -25,3 +25,27 @@ export function Dashboard() {
                     </Link>
                 </div>
             </section>
+
+            <nav id={Style.nav} className={Style.nav_menu}>
+                <ul className="nav flex-column">
+                    <li className={Style.nav_item}>
+                        <Link id={Style.active} className={Style.nav_links} aria-current="page" to="#"><i className="bi bi-book" aria-hidden="true"></i> About</Link>
+                    </li>
+                    <li className={Style.nav_item}>
+                        <Link className={Style.nav_links} to="#"><i className="bi bi-plus-square" aria-hidden="true"></i> Create Combo</Link>
+                    </li>
+                    <li className={Style.nav_item}>
+                        <Link className={Style.nav_links} to="#"><i className="bi bi-journals" aria-hidden="true"></i> View Combo</Link>
+                    </li>
+                    <li className={Style.nav_item}>
+                        <Link className={Style.nav_links} to="#"><i className="bi bi-people" aria-hidden="true"></i> Community</Link>
+                    </li>
+                    <li className={Style.nav_item}>
+                        <Link className={Style.nav_links} to="#"><i className="bi bi-card-text" aria-hidden="true"></i> Main Page</Link>
+                    </li>
+                    <li className={Style.nav_item}>
+                        <Link id={Style.logout} className={Style.nav_links} to="#"><i className="bi bi-door-open" aria-hidden="true"></i> Log Out</Link>
+                    </li>
+                </ul>
+            </nav>
+        </aside>
