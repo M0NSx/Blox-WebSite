db544cc0f70386357069b96a730b08d6825660d6
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-09-24 18:00:42 -0300

    Update dashboard.js

--- source/src/pages/dashboard.js
+++ source/src/pages/dashboard.js
@@ -2,3 +2,5 @@ import { Link } from "react-router-dom";
 import Style from './dashboard.module.css'
 import ProfileImage from "../img/Site-logo.png";
 import 'bootstrap-icons/font/bootstrap-icons.css';
+
+export function Dashboard() {
